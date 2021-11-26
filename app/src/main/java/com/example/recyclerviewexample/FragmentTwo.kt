770c2e07c1bea72328d55b91afcdca6052405da1
dfchaves <davidchaves@tyba.com.co>
2021-11-25 21:49:55 -0500

    fragments

--- app/src/main/java/com/example/recyclerviewexample/FragmentTwo.kt
+++ app/src/main/java/com/example/recyclerviewexample/FragmentTwo.kt
@@ -8,7 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class FragmentTwo: Fragment() {
+class FragmentTwo : Fragment() {
 
     val TAG = "Fragment Two"
 
--- app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
@@ -3,22 +3,51 @@ package com.example.recyclerviewexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.recyclerviewexample.utils.Utils
-import org.json.JSONException
-import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 //    var cityNames: ArrayList<String> = ArrayList()
 //    var cityContact: ArrayList<String> = ArrayList()
 //    var cityDescription: ArrayList<String> = ArrayList()
-    var isFrameOneLoaded : Boolean = true
-    val manager = supportFragmentManager
+
+    private var ifFragmentOneLoaded = true;
+    private val manager = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        val change = findViewById<Button>(R.id.btn_change)
+        showFragmentOne()
+        change.setOnClickListener {
+            if (ifFragmentOneLoaded) {
+                showFragmentTwo()
+            } else {
+                showFragmentOne()
+            }
+        }
+    }
+
+    private fun showFragmentOne(){
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentOne()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        ifFragmentOneLoaded = true
+    }
+    private fun showFragmentTwo(){
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentTwo()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        ifFragmentOneLoaded = false
+    }
+}
+
+
+
+
+// val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
 //        val linearLayoutManager = LinearLayoutManager(applicationContext)
 //
 //        recyclerView.layoutManager = linearLayoutManager
@@ -41,33 +70,3 @@ class MainActivity : AppCompatActivity() {
 //        val flowerList = Utils(this).getFlowersList()
 //        recyclerView.adapter = FlowerAdapter(flowerList)
 //        println(flowerList)
-
-        val change  = findViewById<Button>(R.id.btn_change)
-        ShowFragmentOne()
-        change.setOnClickListener {
-            if (isFrameOneLoaded) {
-                ShowFragmentTwo()
-            } else {
-                ShowFragmentOne()
-            }
-        }
-    }
-
-    fun ShowFragmentOne () {
-        val transaction = manager.beginTransaction()
-        val fragment = FragmentOne()
-        transaction.replace(R.id.fragment_holder, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
-        isFrameOneLoaded = true
-    }
-
-    fun ShowFragmentTwo () {
-        val transaction = manager.beginTransaction()
-        val fragment = FragmentTwo()
-        transaction.replace(R.id.fragment_holder, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
-        isFrameOneLoaded = false
-    }
-}
